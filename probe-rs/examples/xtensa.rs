8f4e8b44ebd09a4d8b602eecc3761ffa0f1c9cb8
Scott Mabin <scott@mabez.dev>
2023-11-14 23:48:50 +0000

    calc chain params for s3

--- probe-rs/examples/xtensa.rs
+++ probe-rs/examples/xtensa.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use probe_rs::Probe;
+use probe_rs::config::ScanChainElement;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -12,6 +13,16 @@ fn main() -> Result<()> {
 
     probe.set_speed(100)?;
     probe.select_protocol(probe_rs::WireProtocol::Jtag)?;
+    probe.set_scan_chain(vec![
+        ScanChainElement {
+            ir_len: Some(5),
+            name: Some("main".to_owned()),
+        },
+        ScanChainElement {
+            ir_len: Some(5),
+            name: Some("second".to_owned()),
+        },
+    ])?;
     probe.attach_to_unspecified()?;
     let _iface = probe
         .try_into_xtensa_interface()
